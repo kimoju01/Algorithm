6bbd5e5a1318f51f5441c4a320a13ac192c1be9a
Hyeju Kim <43704446+kimoju01@users.noreply.github.com>
2024-03-15 18:16:25 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int service = 0;
+        
+        service = n/10;
+        
+        answer = (12000 * n) + (2000 * (k - service)); 
+        
+        return answer;
+    }
+}
