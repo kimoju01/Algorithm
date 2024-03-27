5e35e6b0d41cad67efacb93a877919bf2019db98
Hyeju Kim <43704446+kimoju01@users.noreply.github.com>
2024-03-27 19:26:13 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.50 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        Arrays.sort(array);
+        int midIdx = array.length / 2;
+        
+        return array[midIdx];
+    }
+}
