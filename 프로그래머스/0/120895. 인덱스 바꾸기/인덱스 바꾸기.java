ffa6220f2b57645c2c6e5771ebf6d2193ab97c55
Hyeju Kim <43704446+kimoju01@users.noreply.github.com>
2024-04-16 17:57:12 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,12 +1,11 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
         StringBuilder sb = new StringBuilder(my_string);
-        
-        char ch1 = sb.charAt(num1);
-        char ch2 = sb.charAt(num2);
-        sb.setCharAt(num1, ch2);
-        sb.setCharAt(num2, ch1);
-        
+
+        char temp = sb.charAt(num2);
+        sb.setCharAt(num2, sb.charAt(num1));
+        sb.setCharAt(num1, temp);
+
         return sb.toString();
     }
 }
