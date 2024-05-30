bb5b922a90f387aea9b481d814979567b341fd72
Hyeju Kim <43704446+kimoju01@users.noreply.github.com>
2024-05-30 17:15:55 +0900

    [Silver V] Title: 셀프 넘버, Time: 156 ms, Memory: 14612 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -1,3 +1,5 @@
+/*
+
 public class Main {
 
     static int max = 10000;
@@ -30,4 +32,40 @@ public class Main {
         }
 
     }
+}
+
+*/
+
+class Main {
+	
+	static int N = 10000;
+	static int[] hasConst = new int[N + 1];	// hasConst[10001] 배열 생성
+	
+	public static void main(String[] args) {
+		
+		for (int i=1; i<=N; i++) {
+			checkSelfNumber(i);			// 1부터 10000까지 검사해서
+		}
+		
+		for (int i=1; i<=N; i++) {
+			if (hasConst[i] == 0)		// 셀프 넘버가 아닌 숫자만 출력해준다
+				System.out.println(i);
+		}
+		
+	}
+	
+	// number가 생성자인 숫자 체크
+	public static void checkSelfNumber (int number) {
+		
+		int sum = number;			// 매개변수 number를 sum에 대입한다
+		
+		while (number != 0) {		// number가 0이 아니라면
+			int mod = number % 10;	// number를 한 자리씩 분리한다
+			number /= 10;
+			sum += mod;				// sum = number 이기 때문에 원래 숫자에 각 자리 수를 더해준 것과 같다
+		}
+		
+		if (sum <= N)				// sum 값이 10000보다 작거나 같으면 (즉, 10000보다 작거나 같은 셀프 넘버라면)
+			hasConst[sum] = 1;		// hasConst 배열 각 인덱스에 1을 대입해준다
+	}
 }
