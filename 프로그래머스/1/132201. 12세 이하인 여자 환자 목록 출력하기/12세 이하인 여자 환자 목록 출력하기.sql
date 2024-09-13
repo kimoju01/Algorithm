e1afb5651632df08fdb882fd0c6d7b961d313fff
Hyeju Kim <43704446+kimoju01@users.noreply.github.com>
2024-09-13 13:13:26 +0900

    [level 1] Title: 12세 이하인 여자 환자 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT pt_name, 
+    pt_no, 
+    gend_cd, 
+    age, 
+    if(tlno is null, "NONE", tlno) tlno
+from patient
+where gend_cd='W' and age <= 12
+order by age desc, pt_name asc;
